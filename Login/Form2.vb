197f36f379d6f26436dc069f448ffb7e40170c49
RodrigoSMendes <63783589+RodrigoSMendes@users.noreply.github.com>
2020-04-23 09:49:33 +0100

    Add files via upload

---
+++ Login/Form2.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form2
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = "admin" And TextBox2.Text = "admin" Then
+            MsgBox("Bem Vindo!")
+        Else
+            MsgBox("Tente Outra Vez!")
+        End If
+    End Sub
+End Class
